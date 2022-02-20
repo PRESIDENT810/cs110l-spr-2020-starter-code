3aa621e9d26d4d9e18d0c07bebc62688ebd0436a
Jacket <117010420@link.cuhk.edu.cn>
2022-02-20 21:02:28 +0800

    Project 2 milestone 1 done

---
+++ proj-2/balancebeam/src/proxy.rs
@@ -0,0 +1,174 @@
+use crate::request;
+use crate::response;
+
+use threadpool::ThreadPool;
+
+use rand::{Rng, SeedableRng};
+use std::net::{TcpListener, TcpStream};
+use crate::CmdOptions;
+
+/// Contains information about the state of balancebeam (e.g. what servers we are currently proxying
+/// to, what servers have failed, rate limiting counts, etc.)
+///
+/// You should add fields to this struct in later milestones.
+#[derive(Clone)]
+struct ProxyState {
+    /// How frequently we check whether upstream servers are alive (Milestone 4)
+    #[allow(dead_code)]
+    active_health_check_interval: usize,
+    /// Where we should send requests when doing active health checks (Milestone 4)
+    #[allow(dead_code)]
+    active_health_check_path: String,
+    /// Maximum number of requests an individual IP can make in a minute (Milestone 5)
+    #[allow(dead_code)]
+    max_requests_per_minute: usize,
+    /// Addresses of servers that we are proxying to
+    upstream_addresses: Vec<String>,
+}
+
+pub struct Proxy{
+    state: ProxyState,
+    bind: String,
+}
+
+impl Proxy{
+    pub fn new(options: CmdOptions) -> Proxy{
+        let state = ProxyState {
+            upstream_addresses: options.upstream,
+            active_health_check_interval: options.active_health_check_interval,
+            active_health_check_path: options.active_health_check_path,
+            max_requests_per_minute: options.max_requests_per_minute,
+        };
+        return Proxy{state, bind: options.bind};
+    }
+
+    pub fn start(&self){
+        // Start listening for connections
+        let listener = match TcpListener::bind(&self.bind) {
+            Ok(listener) => listener,
+            Err(err) => {
+                log::error!("Could not bind to {}: {}", &self.bind, err);
+                std::process::exit(1);
+            }
+        };
+        log::info!("Listening for requests on {}", &self.bind);
+
+        let n_workers = 8;
+        let thread_pool = ThreadPool::new(n_workers);
+
+        for stream in listener.incoming() {
+            let state = self.state.clone();
+            if let Ok(stream) = stream {
+                // Handle the connection!
+                thread_pool.execute(move || {
+                    handle_connection(&state, stream);
+                })
+            }
+        }
+    }
+}
+
+// setup connection with a random upstream server
+fn connect_to_upstream(state: &ProxyState) -> Result<TcpStream, std::io::Error> {
+    let mut rng = rand::rngs::StdRng::from_entropy();
+    let upstream_idx = rng.gen_range(0, state.upstream_addresses.len());
+    let upstream_ip = &state.upstream_addresses[upstream_idx];
+    TcpStream::connect(upstream_ip).or_else(|err| {
+        log::error!("Failed to connect to upstream {}: {}", upstream_ip, err);
+        Err(err)
+    })
+    // TODO: implement failover (milestone 3)
+}
+
+// forward the response received from upstream server to client
+fn send_response(client_conn: &mut TcpStream, response: &http::Response<Vec<u8>>) {
+    let client_ip = client_conn.peer_addr().unwrap().ip().to_string();
+    log::info!("{} <- {}", client_ip, response::format_response_line(&response));
+    if let Err(error) = response::write_to_stream(&response, client_conn) {
+        log::warn!("Failed to send response to client: {}", error);
+        return;
+    }
+}
+
+// handle a connection from client
+fn handle_connection(state: &ProxyState, mut client_conn: TcpStream) {
+    let client_ip = client_conn.peer_addr().unwrap().ip().to_string();
+    log::info!("Connection received from {}", client_ip);
+
+    // Open a connection to a random destination server
+    let mut upstream_conn = match connect_to_upstream(state) {
+        Ok(stream) => stream,
+        Err(_error) => {
+            let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
+            send_response(&mut client_conn, &response);
+            return;
+        }
+    };
+    let upstream_ip = client_conn.peer_addr().unwrap().ip().to_string();
+
+    // The client may now send us one or more requests. Keep trying to read requests until the
+    // client hangs up or we get an error.
+    loop {
+        // Read a request from the client
+        let mut request = match request::read_from_stream(&mut client_conn) {
+            Ok(request) => request,
+            // Handle case where client closed connection and is no longer sending requests
+            Err(request::Error::IncompleteRequest(0)) => {
+                log::debug!("Client finished sending requests. Shutting down connection");
+                return;
+            }
+            // Handle I/O error in reading from the client
+            Err(request::Error::ConnectionError(io_err)) => {
+                log::info!("Error reading request from client stream: {}", io_err);
+                return;
+            }
+            Err(error) => {
+                log::debug!("Error parsing request: {:?}", error);
+                let response = response::make_http_error(match error {
+                    request::Error::IncompleteRequest(_)
+                    | request::Error::MalformedRequest(_)
+                    | request::Error::InvalidContentLength
+                    | request::Error::ContentLengthMismatch => http::StatusCode::BAD_REQUEST,
+                    request::Error::RequestBodyTooLarge => http::StatusCode::PAYLOAD_TOO_LARGE,
+                    request::Error::ConnectionError(_) => http::StatusCode::SERVICE_UNAVAILABLE,
+                });
+                send_response(&mut client_conn, &response);
+                continue;
+            }
+        };
+        log::info!(
+            "{} -> {}: {}",
+            client_ip,
+            upstream_ip,
+            request::format_request_line(&request)
+        );
+
+        // Add X-Forwarded-For header so that the upstream server knows the client's IP address.
+        // (We're the ones connecting directly to the upstream server, so without this header, the
+        // upstream server will only know our IP, not the client's.)
+        request::extend_header_value(&mut request, "x-forwarded-for", &client_ip);
+
+        // Forward the request to the server
+        if let Err(error) = request::write_to_stream(&request, &mut upstream_conn) {
+            log::error!("Failed to send request to upstream {}: {}", upstream_ip, error);
+            let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
+            send_response(&mut client_conn, &response);
+            return;
+        }
+        log::debug!("Forwarded request to server");
+
+        // Read the server's response
+        let response = match response::read_from_stream(&mut upstream_conn, request.method()) {
+            Ok(response) => response,
+            Err(error) => {
+                log::error!("Error reading response from server: {:?}", error);
+                let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
+                send_response(&mut client_conn, &response);
+                return;
+            }
+        };
+        // Forward the response to the client
+        send_response(&mut client_conn, &response);
+        log::debug!("Forwarded response to client");
+    }
+}
